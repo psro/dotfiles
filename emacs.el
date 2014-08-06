3025082ebd0a15e7233271ae2926d23aae33a1b1
Samuel Otter <samuel.otter@gmail.com>
2014-08-06 11:04:31 +0200

    Updated custom settings in emacs.el

--- emacs.el
+++ emacs.el
@@ -61,7 +61,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("cd70962b469931807533f5ab78293e901253f5eeb133a46c2965359f23bfb2ea" default))))
+ '(custom-safe-themes (quote ("cd70962b469931807533f5ab78293e901253f5eeb133a46c2965359f23bfb2ea" default)))
+ '(safe-local-variable-values (quote ((allout-layout . t)))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
