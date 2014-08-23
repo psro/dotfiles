3c257a704dc54f4e18704a863528419af5af0484
Samuel Otter <samuel@otter.se>
2014-08-23 11:42:10 +0200

    Change emacs keybindings for windmove.

--- emacs.el
+++ emacs.el
@@ -30,7 +30,10 @@
 (load-theme 'zenburn t)
 
 ;; Windmove
-(windmove-default-keybindings 'meta)
+(global-set-key (kbd "C-M-<left>")  'windmove-left)
+(global-set-key (kbd "C-M-<right>") 'windmove-right)
+(global-set-key (kbd "C-M-<up>")    'windmove-up)
+(global-set-key (kbd "C-M-<down>")  'windmove-down)
 
 ;; Whitespace mode
 (setq whitespace-style
