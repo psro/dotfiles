8e01953cb2adc7431df6bdd123b128348a27ebcf
Samuel Otter <samuel@otter.se>
2014-08-23 11:47:36 +0200

    Move local and custom settings out of emacs.el

--- emacs.el
+++ emacs.el
@@ -75,22 +75,3 @@
   (global-set-key "\C-cc" 'org-capture)
   (global-set-key "\C-ca" 'org-agenda)
   (global-set-key "\C-cb" 'org-iswitchb))
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("cd70962b469931807533f5ab78293e901253f5eeb133a46c2965359f23bfb2ea" default)))
- '(org-agenda-files (quote ("~/notes/gbl-11627.org")))
- '(safe-local-variable-values (quote ((allout-layout . t)))))
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
-(provide '.emacs)
-;;; .emacs ends here
