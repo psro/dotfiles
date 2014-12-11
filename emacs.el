0ca8e6afb4e38e675dc38c0389afa32f236e31d2
Samuel Otter <samuel@otter.se>
2014-12-11 20:57:03 +0100

    Autoload dired-jump

--- emacs.el
+++ emacs.el
@@ -108,3 +108,13 @@
 
 ;; Align Regexp keybindings
 (global-set-key "\C-xa" 'align-regexp)
+
+
+;; Dired related
+(autoload 'dired-jump "dired-x"
+  "Jump to Dired buffer corresponding to current buffer." t)
+(autoload 'dired-jump-other-window "dired-x"
+  "Like \\[dired-jump] (dired-jump) but in other window." t)
+
+(define-key global-map "\C-x\C-j" 'dired-jump)
+(define-key global-map "\C-x4\C-j" 'dired-jump-other-window)
